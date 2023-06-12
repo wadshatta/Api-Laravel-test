cc447aa3e162acca16b87bb692b716d4d805154e
fakhareldin abakar <39832576+wadshatta@users.noreply.github.com>
2023-06-12 19:46:18 +0500

    this commit after connect the project with database

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dummyAPI;
+use App\Http\Controllers\DeviceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get("data",[dummyAPI::class,"getData"]);
+Route::get('list',[DeviceController::class,"list"]);
